28b982400d955a3ab3be11709df6b624866739c5
Christophe Kumsta <padonion@gmail.com>
2023-05-08 23:30:40 +0000

    Configure username and modify C++ programs to handle it

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -18,5 +18,9 @@ void test_validate_my_username()
      * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
      * config file and my_username() functions are setup properly
      */
-    TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+    const char *config_name = my_username();
+    printf("my_usernane: %s",config_name);
+    char *reference_name = malloc_username_from_conf_file();
+    printf("malloc...: %s", reference_name);
+    TEST_ASSERT_EQUAL_STRING_MESSAGE(config_name, reference_name, "The file config/username.txt should contain your github username!");
 }
