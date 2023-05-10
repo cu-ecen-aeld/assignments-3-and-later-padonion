fd2ef0bf25d840750b9589cb9df0cf5d76490a2b
Christophe Kumsta <padonion@gmail.com>
2023-05-10 02:22:32 +0000

    Make the programs running as expected

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -9,17 +9,17 @@ int main(int argc, char *argv[]) {
 
     // get argument numbers (do not forget argv[0] which is the executable name)
     if(argc != 3) {
-        syslog(LOG_ERR, "Requires 2 arguments:\n writestr: string to write\n writefile: file to be written");
+        syslog(LOG_ERR, "Requires 2 arguments:\n writefile: file to be written\n writestr: string to write");
         return 1;
     }
-    
-    FILE *fp = fopen(argv[2], "w");
+
+    FILE *fp = fopen(argv[1], "w");
     if(fp == NULL) {
         syslog(LOG_ERR, "Error opening file to write: %s", strerror(errno));
         return 1;
     }
 
-    fprintf(fp, "%s", argv[1]);
+    fprintf(fp, "%s", argv[2]);
     fclose(fp);
 
     return 0;
